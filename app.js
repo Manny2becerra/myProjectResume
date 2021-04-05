03e321a7ad6e6d02d3a09d119e23efced54c111b
Manny2becerra <71099429+Manny2becerra@users.noreply.github.com>
2021-04-05 11:35:48 -0400

    Final commit

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // creating server
 const express = require('express');
 const app = express();
-
 // adds necessary middleware 
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -11,17 +10,14 @@ app.set('view engine', 'pug');
 const routes = require('./routes/index.js');
 
 // handles routes
+app.use('/static', express.static('public'));
 app.use(routes);
 
 // handles errors for no page found
 app.use((req, res, next) => {
-    const err = new Error('Sorry! This page doesnt exist');
-    err.status = 400;
-    next(err);
-});
-
-app.use((err, req, res, next) => {
-    res.render('/err', {message: err.message, status: err.status});
+    const err = new Error('Oh no! You searched for page that does not exist!');
+    err.status = 404;
+   res.render('error', { err });
 });
 
 app.listen(3000, () => console.log('The server is running'));
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,22 @@ router.get('/', (req, res) => {
     res.render('index', {projects});
 });
 
+router.get('/about', (req, res) => {
+    res.render('about');
+});
+
+router.get('/project', (req, res) => {
+    const num = Math.floor(Math.random()*projects.length);
+    res.redirect(`/project/${num}`);
+});
+
+router.get('/project/:id', (req, res) => {
+    const id = +req.params.id;
+    console.log(id);
+    if (!projects[id]) {
+        return res.redirect('/error');
+    }
+    res.render('project', {project: projects[id]});
+});
+
 module.exports = router;
